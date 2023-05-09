57e0728d40bdac384d6ceed999441601cdd3b355
Thiago Leite <thiago.leite@agileengine.com>
2023-05-09 11:40:03 -0300

    Using useEffect hook to handling token updates and expiration

--- frontend/src/pages/Root/Root.js
+++ frontend/src/pages/Root/Root.js
@@ -1,9 +1,35 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
 import { MainNavigation } from "../../components";
+import { useEffect } from "react";
+import { getTokenDuration } from "../../utils";
 
 const RootLayout = () => {
   const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "POST" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,19 +1,44 @@
 import { redirect } from "react-router-dom";
 
-const STORE_KEY = "token";
+const TOKEN_KEY = "token";
+const EXPIRATION_KEY = "expiration";
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem(EXPIRATION_KEY);
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+
+  return duration;
+}
 
 export function getAuthToken() {
-  const token = localStorage.getItem(STORE_KEY);
+  const token = localStorage.getItem(TOKEN_KEY);
+  const tokenDuration = getTokenDuration();
+
+  if (!token) {
+    return null;
+  }
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
 
   return token;
 }
 
 export function setAuthToken(token) {
-  localStorage.setItem(STORE_KEY, token);
+  localStorage.setItem(TOKEN_KEY, token);
+
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+
+  localStorage.setItem(EXPIRATION_KEY, expiration.toISOString());
 }
 
 export function removeAuthToken() {
-  localStorage.clear(STORE_KEY);
+  localStorage.clear(TOKEN_KEY);
+  localStorage.clear(EXPIRATION_KEY);
 }
 
 export function tokenLoader() {
